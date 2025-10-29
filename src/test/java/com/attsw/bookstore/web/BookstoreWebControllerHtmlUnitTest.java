a4eb8efd34107ffe726a8c3292dd714a683713ae
AvanAvi <theastrofreek@gmail.com>
2025-10-29 16:48:01 +0100

    refactor: align test package structure with main source structure

--- src/test/java/com/attsw/bookstore/web/BookstoreWebControllerHtmlUnitTest.java
+++ src/test/java/com/attsw/bookstore/web/BookstoreWebControllerHtmlUnitTest.java
@@ -1,4 +1,4 @@
-package com.attsw.bookstore.controller.web;
+package com.attsw.bookstore.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
--- src/test/java/com/attsw/bookstore/web/CategoryWebControllerWebMvcTest.java
+++ src/test/java/com/attsw/bookstore/web/CategoryWebControllerWebMvcTest.java
@@ -1,4 +1,4 @@
-package com.attsw.bookstore.controller.web;
+package com.attsw.bookstore.web;
 
 import com.attsw.bookstore.web.CategoryWebController;
 import com.attsw.bookstore.service.CategoryService;
